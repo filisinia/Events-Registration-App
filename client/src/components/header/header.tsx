3f6cfd6990b63cb983cf2f58c967948a6e01a966
Mariia Petrenko <filisinia@gmail.com>
2024-05-16 17:01:07 +0300

    feat: create a search field

--- client/src/components/header/header.tsx
+++ client/src/components/header/header.tsx
@@ -1,13 +1,19 @@
-import { AppBar, Button, Toolbar, Typography } from "@mui/material";
+import { AppBar, Box, Button, Toolbar, Typography } from "@mui/material";
 import { Link } from "react-router-dom";
+import Search from "./search";
+import styles from "./headerStyle";
 
 export default function Header({ content }: { content: 'Events' | 'Participants' }): JSX.Element {
   return (
     <>
     <AppBar component='nav'>
-      <Toolbar sx={{ display: 'flex', justifyContent: 'space-between' }}>
+      <Toolbar sx={styles.toolbar}>
         <Typography variant='h5'>{content}</Typography>
-        { content === 'Participants' ? <Button component={Link} to='/' color='success' variant='contained'>Back</Button> : ''}
+        { content === 'Participants' ?
+        <Box sx={styles.controllers}>
+          <Search />
+          <Button component={Link} to='/' color='success' variant='contained'>Back</Button>
+        </Box> : ''}
       </Toolbar>
     </AppBar>
     <Toolbar />
